4b5c4e02b69c3b47cd227cccd6cef40d11134bf0
Yernar Smagulov <yernar.smagulov02@gmail.com>
2023-07-07 15:22:22 -0700

    all sprint 1 functionality is done, needs work on the visual part

--- pantrypal_v2.py
+++ pantrypal_v2.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import requests
+import json
+import pandas as pd
 
 def get_recipes(ingredient_list):
     # Replace with the actual URL of your Flask API
-    # url = "http://127.0.0.1:5000/rec"
-    url = "http://localhost:5000/rec" #not working for now
+    url = "http://127.0.0.1:5000/rec"
+    # Prepare the data in the required format
+    data = {"ingredients": ingredient_list}
+    # Convert dict to json
+    data_json = json.dumps(data)
+    headers = {'Content-type':'application/json'}
 
-    # Convert the list of ingredients into a format that can be sent in a GET request
-    params = {"ingredients": ','.join(ingredient_list)}
-    response = requests.get(url, params=params)
+    response = requests.post(url, data=data_json, headers=headers)
 
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        df = pd.DataFrame.from_dict(data)
+        df.head()
+        # print(response.json())
+        return df.head()
     else:
         st.error(f"Error when trying to get recipes: {response.status_code}")
 
@@ -20,25 +28,26 @@ def main():
 
     # Create a text input box for the user to enter ingredients
     ingredients = st.text_area("Enter the list of ingredients (one per line):")
+    ingredient_list = []
 
     # Create a button to submit the ingredients
     if st.button("Submit"):
         # Split the user input into a list of ingredients
         ingredient_list = ingredients.split('\n')
         
-        # Get recipe recommendations
+    # Get recipe recommendations
+    if (ingredient_list):
         recipes = get_recipes(ingredient_list)
-
         # Print the list of ingredients back to the user
         st.write("You submitted the following ingredients:")
         for ingredient in ingredient_list:
             st.write(ingredient)
 
         # Display the recipe recommendations
-        if recipes:
-            st.write("Here are your recipe recommendations:")
-            for recipe in recipes:
-                st.write(recipe)
+        st.write("Here are your recipe recommendations:")
+        for i in recipes.index:
+            # print(recipes['recipe_urls'][ind], recipes['recipe_name'][ind])
+            st.write(recipes['recipe_urls'][i], recipes['recipe_name'][i], recipes['ingredients'][i])
 
 if __name__ == "__main__":
     main()
