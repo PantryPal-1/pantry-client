8ab845bb1aec6883a7fa26b428dce0f0e4a82230
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-07-24 20:42:21 -0700

    hotfix: fixed filter and port num to 5000

--- pantryPal_v7.py
+++ pantryPal_v7.py
@@ -9,8 +9,8 @@ COMMON_INGREDIENTS = ["Tomato", "Cheese", "Bread", "Butter", "Egg", "Milk", "Oni
 
 def get_recipes(ingredient_list, num_recipes, filter_option):
     try:
-        url = f"http://127.0.0.1:4999/rec?use_rec=true&n={num_recipes}"  # update the URL to handle the number of recipes
-        if filter_option == "vegetarian":
+        url = f"http://127.0.0.1:5000/rec?use_rec=true&n={num_recipes}"  # update the URL to handle the number of recipes
+        if filter_option == "Vegetarian":
             url += "&is_veg=true"
         elif filter_option == "Nut-free":
             url += "&is_nut_free=true"
